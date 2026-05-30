8e30c0314e1d074cd43efb296c13e61dd80128dd
nyndaa <raharjiansustanadiawijayanti@gmail.com>
2026-05-31 00:57:25 +0700

    Session 3

--- app/(landing)/components/layouts/header.tsx
+++ app/(landing)/components/layouts/header.tsx
@@ -5,12 +5,14 @@ import Link from "next/link";
 import { FiSearch, FiShoppingBag } from "react-icons/fi";
 import CartPopup from "../ui/cart-popup";
 import { useState } from "react";
+import { useCartStore } from "@/app/hooks/use-cart-store";
 
 const Header = () => {
   const [isCartPopupOpen, setIsCartPopupOpen] = useState(false);
+const items = useCartStore((state) => state.items);
 
   return (
-    <header>
+    <header className="fixed w-full z-20 backdrop-blur-3xl bg-white/50">
       <div className="flex justify-between gap-10 container mx-auto py-7">
         <Link href="/">
           <Image
@@ -37,9 +39,13 @@ const Header = () => {
             onClick={() => setIsCartPopupOpen(!isCartPopupOpen)}
           >
             <FiShoppingBag size={24} />
+            {items.length ?(
             <div className="bg-primary rounded-full w-3.5 h-3.5 absolute -top-1 -right-1 text-[10px] text-white text-center">
-              3
+              {items.length}
             </div>
+            ) : (
+              <></>
+            )}
           </button>
           {isCartPopupOpen && <CartPopup />}
         </div>
--- app/(landing)/components/product-detail/product-actions.tsx
+++ app/(landing)/components/product-detail/product-actions.tsx
@@ -10,6 +10,10 @@ import Button from "../ui/button";
 import { useState } from "react";
 import { useRouter } from "next/navigation";
 
+type ProductActionsProps = {
+  stock: number;
+};
+
 const ProductActions = () => {
   const { push } = useRouter();
   const [qty, setQty] = useState(1);
@@ -25,7 +29,7 @@ const ProductActions = () => {
         <div className="flex flex-col">
           <button
             className="border-b border-gray-500 cursor-pointer h-1/2 aspect-square flex items-center justify-center"
-            onClick={() => setQty(qty + 1)}
+            onClick={() => setQty(qty < stock ? qty + 1 : qty)}
           >
             <FiChevronUp />
           </button>
